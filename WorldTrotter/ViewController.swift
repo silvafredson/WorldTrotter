1a72a8559ee674b7ce5d2e0775b77a12ae88b15b
fredson-silva-pessoal <silva.fredson@hotmail.com>
2023-09-26 15:25:19 -0300

    setting up the project

---
+++ WorldTrotter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Fredson Silva on 26/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+
+
+}
+
